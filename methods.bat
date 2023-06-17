957872415d9a679f2c34cb7f2e3084df7ca2608e
Bala Naren Chanumolu <4568naren@gmail.com>
2023-06-17 03:15:35 -0400

    4th

--- methods.bat
+++ methods.bat
@@ -1,8 +1,9 @@
+@echo off
 python generateUniqueProjects.py
 mkdir process
 move  genMethods.py process
 move  run.sh process
 move  UniqueProjects.csv process
 cd process
-run.bat
+call run.bat
 python genMethods.py
