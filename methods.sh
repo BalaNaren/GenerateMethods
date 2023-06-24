16bb0bb2ffbd831e1ef4e7aa66889a582a0728f1
BalaNaren <52159396+BalaNaren@users.noreply.github.com>
2023-06-23 20:50:37 -0400

    Update methods.sh

--- methods.sh
+++ methods.sh
@@ -4,6 +4,7 @@ cp  genMethods.py process
 cp  run.sh process
 cp  UniqueProjects.csv process
 cp all-polluter-cleaner-info-combined-filtered-fp.csv process
+cp processedOrgCsv_P7.csv process
 cd process
 bash run.sh
 python3 genMethods.py
